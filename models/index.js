9da7d913261afcff9814fc1cf124c9f53ff2cab5
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-05-24 19:59:55 -0700

    created post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
+const Post = require("./Post");
 
 
 
@@ -14,6 +15,5 @@ const User = require('./User');
 
 
 
-
-module.exports = { User };
+module.exports = { User, Post };
 
