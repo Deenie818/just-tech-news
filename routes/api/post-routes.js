af5fbc2bc08c1f750a036c45b8244cd4f7ccc4dc
Deenie818 <99102981+Deenie818@users.noreply.github.com>
2022-05-30 13:46:33 -0700

    added style.css

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,4 +1,4 @@
-cconst router = require('express').Router();
+const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
 
